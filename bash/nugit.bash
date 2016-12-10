6cfdff7ea1381cc7d291a1095f94cf246b8f3303
matthewoden <heymatthewoden@gmail.com>
2016-12-10 09:08:06 -0600

    cleanup nuget and mkcd

--- bash/nugit.bash
+++ bash/nugit.bash
@@ -1,9 +1,8 @@
 # requires mkcd
 nugit() {
-  mkcd $1 &&
-  touch .gitignore &&
-  touch README.md &&
-  echo "# $1" &&
-  git init &&
-  git status
+  mkcd $1 && # make directory
+  echo ".DS_Store" >> .gitignore # basic ignore
+  echo "# $1" >> ./README.md # pipe the directory name in to the readme title
+  git init && # create the repo
+  git status # show user the results.
 }
